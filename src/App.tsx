2f4edfadf174b8b6c4be27be5002919e20da9ead
dostonabdusattorov <webdeveloperdoston@gmail.com>
2024-01-26 12:47:08 +0500

    project setup, installed needed packages, project architecture, Button component created, header and hero section done

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,42 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.scss";
+import logo from "./assets/images/logo.svg";
+import heroImg from "./assets/images/Group 79.png";
+import { Button } from "./components";
 
-function App() {
-  const [count, setCount] = useState(0);
-
+const App = () => {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header className="header">
+        <div className="header-content container">
+          <div className="logo">
+            <img src={logo} alt="logo" />
+          </div>
+          <nav className="nav">
+            <ul>
+              <li>서비스 소개</li>
+              <li>가격정책</li>
+            </ul>
+            <ul>
+              <li>고객 후기</li>
+              <li>문의하기</li>
+              <li>HOCA 팀</li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+      <section className="hero">
+        <img src={heroImg} alt="hero img" className="hero-img" />
+        <div className="hero-content">
+          <h1>세계적인 개발자 팀을 구독하고 회사의 성장을 가속화하세요!</h1>
+          <p>
+            전세계 엘리트 개발자들로 구성된 호카와 함께, 저렴한 비용으로 최고의
+            팀을 구독해보세요!
+          </p>
+          <Button>문의하기</Button>
+        </div>
+      </section>
     </>
   );
-}
+};
 
 export default App;
---
+++ src/components/Button.tsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+interface Props {}
+
+export const Button = styled.button<Props>`
+  width: 18.4rem;
+  height: 6.1rem;
+  border-radius: 3.05rem;
+  background-color: #7c19ea;
+  box-shadow: 0px 0px 25px 0px #7c19ea6e;
+  color: #fff;
+  text-align: center;
+  font-family: Pretendard;
+  font-size: 2rem;
+  font-weight: 700;
+  border: none;
+  outline: none;
+  cursor: pointer;
+`;
