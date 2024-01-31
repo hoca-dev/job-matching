c25cd52c49e5eb879faea0678895456845585544
dostonabdusattorov <webdeveloperdoston@gmail.com>
2024-01-31 13:59:22 +0500

    footer finished

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,7 @@ import arrowRight from "./assets/images/Vector (1).svg";
 import addFrame1 from "./assets/images/Frame.png";
 import addFrame2 from "./assets/images/Group 52.png";
 import asteriks2 from "./assets/images/Asterisk 2.png";
+import logoDark from "./assets/images/logo 4.svg";
 import { Button, PriceCard } from "./components";
 
 const App = () => {
@@ -259,6 +260,31 @@ const App = () => {
           <p>JOIN WITH HOCA TEAM</p>
         </div>
       </section>
+      <footer className="footer">
+        <div className="footer-logo">
+          <img src={logoDark} alt="logo dark" />
+        </div>
+        <div className="footer-content">
+          <nav className="nav">
+            <ul>
+              <li>회사소개</li>
+              <li>이용약관</li>
+              <li className="selected">개인정보처리방침</li>
+              <li>도움말</li>
+              <li>문의하기</li>
+            </ul>
+          </nav>
+          <div className="desc">
+            <p>대표전화 : +82 02 598-3373</p>
+            <p>
+              서울 서초구 반포대로21길 39 (서초동, 산해빌딩) 402호
+              (대표자:박형준) 사업자등록번호 : 132-87-02761
+            </p>
+            <p>전화번호 : +82 010-4787-3373 ㅣ 이메일 : jun@hoca.dev</p>
+            <p>Copyright 2024. HOCA Company. All Rights Reserved.</p>
+          </div>
+        </div>
+      </footer>
     </>
   );
 };
