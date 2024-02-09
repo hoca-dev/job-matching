6f2280752807076149dc52581d34863c5a103c2f
dostonabdusattorov <webdeveloperdoston@gmail.com>
2024-02-09 14:07:14 +0500

    google authentication finished

--- src/routes/Login/Login.tsx
+++ src/routes/Login/Login.tsx
@@ -1,5 +1,13 @@
 import styled from "styled-components";
 import { LoginComponent } from "../../components";
+import { createClient } from "@supabase/supabase-js";
+import googleIcon from "../../assets/images/icons8-google.svg";
+
+const SUPABASE_KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF6a3lxdGxqc2NvY2d1ZGxsaHNsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDczNzQwMTcsImV4cCI6MjAyMjk1MDAxN30.tBDQODLt3Yv5QMcC5k82KRG7kGVeosfhrHZNROEXSac";
+
+const SUPABASE_URL = "https://azkyqtljscocgudllhsl.supabase.co";
+const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
 const LoginRouteContainer = styled.div`
   width: 100%;
@@ -8,11 +16,35 @@ const LoginRouteContainer = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  .google-btn {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 2rem;
+    font-size: 2rem;
+    padding: 1rem 3rem;
+    position: fixed;
+    top: 2rem;
+    right: 2rem;
+    cursor: pointer;
+  }
 `;
 
 export const LoginRoute = () => {
+  const googleLogin = async () => {
+    await supabase.auth.signInWithOAuth({
+      provider: "google",
+      options: {
+        redirectTo: "http://localhost:5173/dashboard",
+      },
+    });
+  };
   return (
     <LoginRouteContainer>
+      <button className="google-btn" onClick={googleLogin}>
+        <img src={googleIcon} alt="google icon" />
+        <span>Continue with Google</span>
+      </button>
       <LoginComponent />
     </LoginRouteContainer>
   );
--- src/store/apis/auth/auth.api.ts
+++ src/store/apis/auth/auth.api.ts
@@ -1,9 +1,9 @@
-import { createApi } from "@reduxjs/toolkit/query/react";
-import { baseQuery } from "../config";
+import { createApi, fakeBaseQuery } from "@reduxjs/toolkit/query/react";
+// import { baseQuery } from "../config";
 
 export const authApi = createApi({
   reducerPath: "authApi",
-  baseQuery: baseQuery("/auth"),
+  baseQuery: fakeBaseQuery(),
   tagTypes: ["auth"],
   endpoints: ({ mutation }) => ({}),
 });
